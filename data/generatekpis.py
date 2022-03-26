7246fd05693d3d4854fdd5ac4f0e689d2d072d87
RaduADumitru <radu.dumitru@rocketmail.com>
2022-03-27 01:07:50 +0200

    Fix KPI Generator

--- data/generatekpis.py
+++ data/generatekpis.py
@@ -13,7 +13,7 @@ with open('kpis.csv') as csv_kpis:
         for agency in agencies_list:
             filename = "kpis\\" + agency[0] + "_kpi.csv"
             with open(filename, mode= 'w') as kpi_file:
-                file_writer = csv.writer(kpi_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                file_writer = csv.writer(kpi_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
                 file_writer.writerow(['KPI', 'Category'])
                 random.shuffle(kpi_list)
                 nr_kpis = random.randint(int(list_size / 2), list_size)
